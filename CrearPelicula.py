70f5d60da7d5b7b20c976dfce62b549487771452
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-11-08 18:06:04 -0500

    Update CrearPelicula.py

--- CrearPelicula.py
+++ CrearPelicula.py
@@ -1,27 +1,57 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
-    print(event) # Log json en CloudWatch
-    tenant_id = event['body']['tenant_id']
-    pelicula_datos = event['body']['pelicula_datos']
-    nombre_tabla = os.environ["TABLE_NAME"]
-    # Proceso
-    uuidv4 = str(uuid.uuid4())
-    pelicula = {
-        'tenant_id': tenant_id,
-        'uuid': uuidv4,
-        'pelicula_datos': pelicula_datos
-    }
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(nombre_tabla)
-    response = table.put_item(Item=pelicula)
-    # Salida (json)
-    print(pelicula) # Log json en CloudWatch
-    return {
-        'statusCode': 200,
-        'pelicula': pelicula,
-        'response': response
-    }
+    try:
+        print(json.dumps({
+            "tipo": "INFO",
+            "log_datos": {
+                "event": event
+            }
+        }))
+        
+        tenant_id = event['body']['tenant_id']
+        pelicula_datos = event['body']['pelicula_datos']
+        nombre_tabla = os.environ["TABLE_NAME"]
+
+        uuidv4 = str(uuid.uuid4())
+        pelicula = {
+            'tenant_id': tenant_id,
+            'uuid': uuidv4,
+            'pelicula_datos': pelicula_datos
+        }
+
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(nombre_tabla)
+        response = table.put_item(Item=pelicula)
+
+        print(json.dumps({
+            "tipo": "INFO",
+            "log_datos": {
+                "pelicula": pelicula,
+                "response": response
+            }
+        }))
+        
+        return {
+            'statusCode': 200,
+            'pelicula': pelicula,
+            'response': response
+        }
+
+    except Exception as e:
+        print(json.dumps({
+            "tipo": "ERROR",
+            "log_datos": {
+                "error_message": str(e),
+                "event": event
+            }
+        }))
+        
+        return {
+            'statusCode': 500,
+            'error': str(e)
+        }
